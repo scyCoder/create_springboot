6fe4a58d9200e0bb7d1a97437a95eeaa95055a9c
chuanyin.sun <chuanyin.sun@chongplus.cn>
2021-05-31 13:39:14 +0800

    添加HelloController

---
+++ src/main/java/com/example/demo/controller/abstra/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller.abstra;
+
+/**
+ * @author sunchuanyin
+ * @version 1.0
+ * @description
+ * @date 2021/5/31 1:38 下午
+ */
+public class HelloController {
+
+    public void getHello() {
+        System.out.println("hello");
+    }
+}
